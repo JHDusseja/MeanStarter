c182961ff0d419875964183f71172a59682820df
JHDusseja <jhdusseja@gmail.com>
2017-11-17 19:06:56 -0800

    Initial Commit

---
+++ mongo.js
@@ -0,0 +1,16 @@
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/mydb';
+
+// Use connect method to connect to the Server
+var connect = function (cb) {
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to MondoDB");
+        cb(db);
+    });
+};
+
+exports.connect = connect;
